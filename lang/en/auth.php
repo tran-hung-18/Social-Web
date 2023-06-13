da7f91715c7439f61b4cac208bba1e1d2c5ccc80
tranhung <hungth@regit-technology.com>
2023-06-13 17:38:51 +0700

    Update migration

---
+++ lang/en/auth.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'email_unregistered' => 'Your email is not registered',
+    'verify_check_mail' => 'Please check your email to verify your account',
+    'verify_success' => 'Authentication successfully. You can now login',
+    'verified' => 'Your email has been verified. You can now login',
+    'login_success' => 'Login Successfully',
+    'login_error' => 'Account information password is incorrect',
+    'register_success' => 'Register Successfully! Check your email to verify your account',
+    'register_error' => 'Register error',
+    'forgot_password_check_mail' => 'Please check your email to get password',
+    'password_new' => 'Please check gmail to get your new password',
+
+
+    'try_again' => "Please try again",
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <form method="POST" action="{{ route('post-login') }}" class="form-auth form-auth-login">
+        @csrf
+        <div class="form-auth-logo">
+            <img src="{{ Vite::asset('resources/images/LogoRegit.png') }}" alt="">
+            <h4>RT-Blogs</h4>
+        </div>
+        <div class="form-auth-title">
+            <p>Sign in</p>
+        </div>
+        <div class="form-auth-input">
+            <label for="email">Username or email<span>*</span></label>
+            <input type="text" id="email" name="email">
+        </div>
+        <div class="form-auth-input">
+            <label for="password">Password<span>*</span></label>
+            <input type="password" id="password" class="input-password" name="password">
+            @if (session('message'))
+                <span>{{ session('message') }}</span>
+            @endif
+            @error ('email')
+                <br><span class="noti-error">{{ $message }}</span>
+            @enderror
+            @error ('password')
+                <br><span class="noti-error">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="form-auth-password">
+            <div class="remember-password">
+                <div class="checkbox-remember">
+                    <input type="checkbox" name="remember">
+                </div>
+                <p>Remember password</p>
+            </div>
+            <a href="{{ route('forgot-password') }}">Forgot your password?</a>
+        </div>
+        <div class="form-auth-btn">
+            <button type="submit">Login</button>
+            <a href="{{ route('view-register') }}">Don't have an account? Sign up here</a>
+        </div>
+    </form>
+@endsection
